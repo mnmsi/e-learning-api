d4cf81bbf62b7c7544d90c69155308adebcad21a
unknown <shohagm.iotait@gmail.com>
2023-06-08 15:53:21 +0600

    verify mail

--- Modules/Admin/Resources/views/template/aside.blade.php
+++ Modules/Admin/Resources/views/template/aside.blade.php
@@ -4,7 +4,7 @@
         <i class="fas fa-times p-3 cursor-pointer text-secondary opacity-5 position-absolute end-0 top-0 d-none d-xl-none"
            aria-hidden="true" id="iconSidenav"></i>
         <a class="navbar-brand m-0 d-flex justify-content-center p-0 pt-1" href="{{ route('dashboard') }}">
-            <img src="{{asset('assets/img/logo/primary-logo.svg')}}" class="navbar-brand-img h-100" alt="main_logo">
+            <img src="{{asset('assets/img/app_logo.png')}}" class="navbar-brand-img h-100" alt="main_logo">
             <span class="ms-1 font-weight-bold"></span>
         </a>
     </div>
--- app/Http/Controllers/ApiAuthController.php
+++ app/Http/Controllers/ApiAuthController.php
@@ -10,7 +10,9 @@ use App\Http\Requests\FacebookLoginRequest;
 use App\Http\Requests\PasswordResetRequest;
 use App\Http\Requests\RegisterUserRequest;
 use App\Http\Requests\SocialLoginRequest;
+use App\Models\EmailVerify;
 use App\Models\User;
+use App\Notifications\EmailVerificationNotification;
 use App\Notifications\ResetMailNotification;
 use App\Notifications\SendMailNotification;
 use App\Repositories\PasswordReset\PasswordResetRepositoryInterface;
@@ -35,7 +37,7 @@ class ApiAuthController extends Controller
 
     public function __construct(UserRepositoryInterface $userRepo, PasswordResetRepositoryInterface $passwordResetRepo)
     {
-        $this->userRepo          = $userRepo;
+        $this->userRepo = $userRepo;
         $this->passwordResetRepo = $passwordResetRepo;
     }
 
@@ -59,9 +61,9 @@ class ApiAuthController extends Controller
 
             return response()->json([
                 'status' => true,
-                'data'   => [
+                'data' => [
                     'token' => $token->plainTextToken,
-                    'user'  => $user,
+                    'user' => $user,
                 ],
             ]);
 
@@ -74,7 +76,7 @@ class ApiAuthController extends Controller
     {
         DB::beginTransaction();
         try {
-            $insertData             = $request->all();
+            $insertData = $request->all();
             $insertData['password'] = Hash::make($request->password);
 
             if ($request->hasFile('avatar')) {
@@ -96,20 +98,20 @@ class ApiAuthController extends Controller
 
 //                $user->notify(new SendMailNotification());
 //                event(new Registered($user));
+                $this->mailSennd();
 
                 return response()->json([
                     'status' => true,
-                    'data'   => [
+                    'data' => [
                         'token' => $token->plainTextToken,
-                        'user'  => $this->userRepo->findData(Auth::id()),
+                        'user' => $this->userRepo->findData(Auth::id()),
                     ],
                 ]);
 
             } else {
                 return Exceptions::error();
             }
-        }
-        catch (\Throwable $th) {
+        } catch (\Throwable $th) {
             DB::rollback();
             throw new Exceptions();
         }
@@ -118,9 +120,9 @@ class ApiAuthController extends Controller
     public function socialLogin(SocialLoginRequest $request)
     {
         try {
-            $client     = new GuzzleHttpClient();
+            $client = new GuzzleHttpClient();
             $apiRequest = $client->request('GET', 'https://oauth2.googleapis.com/tokeninfo?id_token=' . $request->gmail_token);
-            $userInfo   = json_decode($apiRequest->getBody());
+            $userInfo = json_decode($apiRequest->getBody());
 
             if (!empty($userInfo->sub)) {
 
@@ -129,13 +131,13 @@ class ApiAuthController extends Controller
                 if (!$user) {
 
                     $location = Location::get($request->ip());
-                    $user     = $this->userRepo->insertData([
-                        'name'               => $userInfo->name,
-                        'email'              => $userInfo->email,
-                        'password'           => Hash::make(time()),
-                        'social_uid'         => Hash::make($userInfo->sub),
+                    $user = $this->userRepo->insertData([
+                        'name' => $userInfo->name,
+                        'email' => $userInfo->email,
+                        'password' => Hash::make(time()),
+                        'social_uid' => Hash::make($userInfo->sub),
                         'is_acc_type_update' => 0,
-                        'country'            => $location->countryName ?? null
+                        'country' => $location->countryName ?? null
                     ]);
 
                     $user->markEmailAsVerified();
@@ -151,9 +153,9 @@ class ApiAuthController extends Controller
 
                     return response()->json([
                         'status' => true,
-                        'data'   => [
+                        'data' => [
                             'token' => $token->plainTextToken,
-                            'user'  => $this->userRepo->findData($user->id),
+                            'user' => $this->userRepo->findData($user->id),
                         ],
                     ]);
                 } else {
@@ -163,8 +165,7 @@ class ApiAuthController extends Controller
             } else {
                 return Exceptions::error('Invalid Token!');
             }
-        }
-        catch (\Throwable $th) {
+        } catch (\Throwable $th) {
             throw new Exceptions();
         }
     }
@@ -190,13 +191,13 @@ class ApiAuthController extends Controller
                 if (!$user) {
 
                     $location = Location::get($request->ip());
-                    $user     = $this->userRepo->insertData([
-                        'name'               => $request->name,
-                        'email'              => $userInfo->email,
-                        'password'           => Hash::make(time()),
-                        'social_uid'         => Hash::make($request->token),
+                    $user = $this->userRepo->insertData([
+                        'name' => $request->name,
+                        'email' => $userInfo->email,
+                        'password' => Hash::make(time()),
+                        'social_uid' => Hash::make($request->token),
                         'is_acc_type_update' => 0,
-                        'country'            => $location->countryName ?? null
+                        'country' => $location->countryName ?? null
                     ]);
                     $user->markEmailAsVerified();
 
@@ -211,9 +212,9 @@ class ApiAuthController extends Controller
 
                     return response()->json([
                         'status' => true,
-                        'data'   => [
+                        'data' => [
                             'token' => $token->plainTextToken,
-                            'user'  => $this->userRepo->findData($user->id),
+                            'user' => $this->userRepo->findData($user->id),
                         ],
                     ]);
                 } else {
@@ -222,8 +223,7 @@ class ApiAuthController extends Controller
             } else {
                 return Exceptions::error('Invalid Token!');
             }
-        }
-        catch (\Throwable $th) {
+        } catch (\Throwable $th) {
             throw new Exceptions();
         }
     }
@@ -231,25 +231,25 @@ class ApiAuthController extends Controller
     public function facebookLogin(FacebookLoginRequest $request)
     {
         try {
-            $client     = new GuzzleHttpClient();
+            $client = new GuzzleHttpClient();
             $apiRequest = $client->request('GET', "https://graph.facebook.com/me?fields=name,email&access_token=" . $request->access_token);
-            $userInfo   = json_decode($apiRequest->getBody());
+            $userInfo = json_decode($apiRequest->getBody());
 
             if (!empty($userInfo->id)) {
 
                 $userMail = $userInfo->id . '@facebook.com';
-                $user     = $this->userRepo->whereFirst(['email' => $userInfo->email ?? $userMail]);
+                $user = $this->userRepo->whereFirst(['email' => $userInfo->email ?? $userMail]);
 
                 if (!$user) {
 
                     $location = Location::get($request->ip());
-                    $user     = $this->userRepo->insertData([
-                        'name'               => $userInfo->name,
-                        'email'              => $userInfo->email ?? $userMail,
-                        'password'           => Hash::make(time()),
-                        'social_uid'         => Hash::make($request->access_token),
+                    $user = $this->userRepo->insertData([
+                        'name' => $userInfo->name,
+                        'email' => $userInfo->email ?? $userMail,
+                        'password' => Hash::make(time()),
+                        'social_uid' => Hash::make($request->access_token),
                         'is_acc_type_update' => 0,
-                        'country'            => $location->countryName ?? null
+                        'country' => $location->countryName ?? null
                     ]);
                     $user->markEmailAsVerified();
 
@@ -264,9 +264,9 @@ class ApiAuthController extends Controller
 
                     return response()->json([
                         'status' => true,
-                        'data'   => [
+                        'data' => [
                             'token' => $token->plainTextToken,
-                            'user'  => $this->userRepo->findData($user->id),
+                            'user' => $this->userRepo->findData($user->id),
                         ],
                     ]);
                 } else {
@@ -275,8 +275,7 @@ class ApiAuthController extends Controller
             } else {
                 return Exceptions::error('Invalid Token!');
             }
-        }
-        catch (\Throwable $th) {
+        } catch (\Throwable $th) {
             throw new Exceptions();
         }
     }
@@ -289,8 +288,7 @@ class ApiAuthController extends Controller
             } else {
                 return Exceptions::error('Incorrect old password!');
             }
-        }
-        catch (\Throwable $th) {
+        } catch (\Throwable $th) {
             throw new Exceptions();
         }
     }
@@ -303,8 +301,7 @@ class ApiAuthController extends Controller
                 return ['status' => true];
             }
             return Exceptions::error('Session Expired');
-        }
-        catch (Throwable $exception) {
+        } catch (Throwable $exception) {
             throw new Exceptions();
         }
     }
@@ -322,9 +319,9 @@ class ApiAuthController extends Controller
                 }
 
                 $request['created_at'] = now();
-                $user                  = $this->userRepo->whereFirst(['email' => $request->email]);
-                $request['token']      = encrypt($user->id);
-                $isResetInsert         = $this->passwordResetRepo->insertData(
+                $user = $this->userRepo->whereFirst(['email' => $request->email]);
+                $request['token'] = encrypt($user->id);
+                $isResetInsert = $this->passwordResetRepo->insertData(
                     $request->only($this->passwordResetRepo->getFillable())
                 );
 
@@ -338,8 +335,8 @@ class ApiAuthController extends Controller
 
             if ($request->isMethod('GET')) {
                 $validator = Validator::make($request->all(), [
-                    'token'            => 'required|exists:App\Models\PasswordReset,token',
-                    'password'         => 'required|min:8',
+                    'token' => 'required|exists:App\Models\PasswordReset,token',
+                    'password' => 'required|min:8',
                     'confirm_password' => 'required_with:password|same:password|min:8'
                 ]);
 
@@ -354,9 +351,9 @@ class ApiAuthController extends Controller
                     );
                 }
 
-                $current   = Carbon::now();
+                $current = Carbon::now();
                 $createdAt = Carbon::parse($passReset->created_at);
-                $user      = $this->userRepo->whereFirst(['email' => $passReset->email]);
+                $user = $this->userRepo->whereFirst(['email' => $passReset->email]);
 
                 if ($createdAt->diffInMinutes($current) >= 5) {
                     return Exceptions::validationError("Token is expired!");
@@ -374,8 +371,7 @@ class ApiAuthController extends Controller
                     return Exceptions::error('Something went wrong!');
                 }
             }
-        }
-        catch (\Exception $ex) {
+        } catch (\Exception $ex) {
             throw new Exceptions();
         }
     }
@@ -393,9 +389,21 @@ class ApiAuthController extends Controller
 
             return Exceptions::success("You can register.");
 
-        }
-        catch (\Exception $exception) {
+        } catch (\Exception $exception) {
             throw new Exceptions();
         }
+    }
+
+//    send verify token
+    private function mailSennd()
+    {
+        $token = str_pad(mt_rand(0, 999999), 6, '0', STR_PAD_LEFT);
+        $email_verify = new EmailVerify();
+        $email_verify->user_id = Auth::id();
+        $email_verify->token = $token;
+        $email_verify->expire_at = Carbon::now()->addMinutes(30);
+        $email_verify->save();
+        Auth::user()->notify(new EmailVerificationNotification($token));
+        return 1;
     }
 }
